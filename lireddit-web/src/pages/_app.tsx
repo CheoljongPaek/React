fd4da6ef6a56ddad72963b21d25b96243442f91e
CheoljongPaek <cjpaek0122@gmail.com>
2021-09-27 23:23:12 +0900

    SETTING cache

--- lireddit-web/src/pages/_app.tsx
+++ lireddit-web/src/pages/_app.tsx
@@ -1,13 +1,46 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
-import { createClient, Provider } from 'urql'
+import { createClient, dedupExchange, fetchExchange, Provider } from 'urql'
+import { cacheExchange, Cache, QueryInput } from '@urql/exchange-graphcache';
 
 import theme from '../theme'
+import { LoginMutation, MeDocument, MeQuery, Query } from '../generated/graphql';
+
+function betterupdateQuery<Result, Query>(
+  result: any,
+  cache: Cache,
+  qi: QueryInput,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
 
 const client = createClient({
   url: 'http://localhost:4000/graphql',
   fetchOptions: {
     credentials: "include"
-  }
+  },
+  exchanges: [dedupExchange, cacheExchange({
+    updates: {
+      Mutation: {
+        login: (_result, args, cache, info) => {
+          betterupdateQuery<LoginMutation, MeQuery>(
+            _result,
+            cache,
+            { query: MeDocument },
+            (result, query) => {
+              if (result.login.errors) {
+                return query
+              } else {
+                return {
+                  me: result.login.user
+                }
+              }
+            }
+          );
+        }
+      }
+    }
+  }), fetchExchange]
 });
 
 function MyApp({ Component, pageProps }: any) {
