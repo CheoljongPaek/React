a9f659fa6b1fbfbdf84df869ad1db58c44b964b1
CheoljongPaek <cjpaek0122@gmail.com>
2021-11-20 23:42:57 +0900

    ADD three js model

---
+++ lireddit-web/components/RectangleLoader.tsx
@@ -0,0 +1,40 @@
+import { forwardRef, Box, BoxProps } from "@chakra-ui/react";
+import { Spinner } from "@chakra-ui/spinner";
+
+export const RectangleSpinner = () => (
+  <Spinner
+    size="xl"
+    position="absolute"
+    left="50%"
+    top="50%"
+    ml="calc(0px - var(--spinner-size) / 2)"
+    mt="calc(0px - var(--spinner-size))"
+  />
+);
+
+export const RectangleContainer = forwardRef<BoxProps, "div">(
+  ({ children }, ref) => (
+    <Box
+      ref={ref}
+      className="voxel-dog"
+      m="auto"
+      mt={["-20px", "-60px", "-120px"]}
+      mb={["-40px", "-140px", "-200px"]}
+      w={[280, 480, 640]}
+      h={[280, 480, 640]}
+      position="relative"
+    >
+      {children}
+    </Box>
+  )
+);
+
+const Loader = () => {
+  return (
+    <RectangleContainer>
+      <RectangleSpinner />
+    </RectangleContainer>
+  );
+};
+
+export default Loader;
--- lireddit-web/components/layouts/main.tsx
+++ lireddit-web/components/layouts/main.tsx
@@ -1,12 +1,19 @@
 import { Box, Container } from "@chakra-ui/layout";
 import Head from "next/head";
-import React from "react";
+import React, { forwardRef } from "react";
 import { NextRouter, Router } from "next/router";
 import Navbar2 from "../NavBar2";
+import RectangleLoader from "../RectangleLoader";
+import dynamic from "next/dynamic";
 
 interface mainProps {
   router: Router;
 }
+
+const LoadRectangle = dynamic(() => import("../Rectangle"), {
+  ssr: false,
+  loading: () => <RectangleLoader />,
+});
 
 const Main: React.FC<mainProps> = ({ children, router }) => {
   return (
@@ -15,11 +22,9 @@ const Main: React.FC<mainProps> = ({ children, router }) => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <title>CJ Paek - Homepage</title>
       </Head>
-
       <Navbar2 path={router.asPath} />
-      {/* <Navbar2 /> */}
-
       <Container maxW="container.md" pt={14}>
+        <LoadRectangle />
         {children}
       </Container>
     </Box>
